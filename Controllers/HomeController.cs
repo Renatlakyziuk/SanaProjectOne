740db057678663f9741636c7cd69829175351c8c
RenatLakyziuk <ipz204_lrv@student.ztu.edu.ua>
2023-04-15 19:23:16 +0300

    project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProjectOne.Models;
+
+namespace ProjectOne.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public Task<IActionResult> Index([Bind("Name")] HomePageModel user){
+            return Task.FromResult<IActionResult>(View(user));
+        }
+    }
+}
---
+++ Models/HomePageModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjectOne.Models
+{
+    public class HomePageModel
+    {
+        [Display(Name = "Your name")]
+        [Required(ErrorMessage = "*")]
+        public string Name { get; set; } = "";
+    }
+}
